599942ecf2cffbda99bd7f1d29964bde018f2bee
AlecsPul <pdpdalld@gmail.com>
2026-05-22 17:50:04 +0200

    upsampling.fs and down

--- data/shader_atlas.glsl
+++ data/shader_atlas.glsl
@@ -770,3 +770,118 @@ void main()
 	//calcule the position of the vertex using the matrices
 	gl_Position = u_viewprojection * vec4( v_world_position, 1.0 );
 }
+
+\bloomquad.vs
+#version 330 core
+layout (location = 0) in vec2 aPosition;
+layout (location = 1) in vec2 aTexCoord;
+
+out vec2 texCoord;
+
+void main()
+{
+    gl_Position = vec4(aPosition.x, aPosition.y, 0.0, 1.0);
+    texCoord = aTexCoord;
+}
+
+
+\downsample.fs
+#version 330 core
+
+uniform sampler2D srcTexture;
+uniform vec2 srcResolution;
+
+in vec2 texCoord;
+layout (location = 0) out vec3 downsample;
+
+
+void main()
+{
+    vec2 srcTexelSize = 1.0 / srcResolution;
+    float x = srcTexelSize.x;
+    float y = srcTexelSize.y;
+
+    // Take 13 samples around current texel:
+    // a - b - c
+    // - j - k -
+    // d - e - f
+    // - l - m -
+    // g - h - i
+    // === ('e' is the current texel) ===
+    vec3 a = texture(srcTexture, vec2(texCoord.x - 2*x, texCoord.y + 2*y)).rgb;
+    vec3 b = texture(srcTexture, vec2(texCoord.x,       texCoord.y + 2*y)).rgb;
+    vec3 c = texture(srcTexture, vec2(texCoord.x + 2*x, texCoord.y + 2*y)).rgb;
+
+    vec3 d = texture(srcTexture, vec2(texCoord.x - 2*x, texCoord.y)).rgb;
+    vec3 e = texture(srcTexture, vec2(texCoord.x,       texCoord.y)).rgb;
+    vec3 f = texture(srcTexture, vec2(texCoord.x + 2*x, texCoord.y)).rgb;
+
+    vec3 g = texture(srcTexture, vec2(texCoord.x - 2*x, texCoord.y - 2*y)).rgb;
+    vec3 h = texture(srcTexture, vec2(texCoord.x,       texCoord.y - 2*y)).rgb;
+    vec3 i = texture(srcTexture, vec2(texCoord.x + 2*x, texCoord.y - 2*y)).rgb;
+
+    vec3 j = texture(srcTexture, vec2(texCoord.x - x, texCoord.y + y)).rgb;
+    vec3 k = texture(srcTexture, vec2(texCoord.x + x, texCoord.y + y)).rgb;
+    vec3 l = texture(srcTexture, vec2(texCoord.x - x, texCoord.y - y)).rgb;
+    vec3 m = texture(srcTexture, vec2(texCoord.x + x, texCoord.y - y)).rgb;
+
+    // Apply weighted distribution:
+    // 0.5 + 0.125 + 0.125 + 0.125 + 0.125 = 1
+    // a,b,d,e * 0.125
+    // b,c,e,f * 0.125
+    // d,e,g,h * 0.125
+    // e,f,h,i * 0.125
+    // j,k,l,m * 0.5
+    // This shows 5 square areas that are being sampled. But some of them overlap,
+    // so to have an energy preserving downsample we need to make some adjustments.
+    // The weights are the distributed, so that the sum of j,k,l,m (e.g.)
+    // contribute 0.5 to the final color output. The code below is written
+    // to effectively yield this sum. We get:
+    // 0.125*5 + 0.03125*4 + 0.0625*4 = 1
+    downsample = e*0.125;
+    downsample += (a+c+g+i)*0.03125;
+    downsample += (b+d+f+h)*0.0625;
+    downsample += (j+k+l+m)*0.125;
+}
+
+\cumsample.fs
+#version 330 core
+uniform sampler2D srcTexture;
+uniform float filterRadius;
+
+in vec2 texCoord;
+layout (location = 0) out vec3 upsample;
+
+void main()
+{
+    // The filter kernel is applied with a radius, specified in texture
+    // coordinates, so that the radius will vary across mip resolutions.
+    float x = filterRadius;
+    float y = filterRadius;
+
+    // Take 9 samples around current texel:
+    // a - b - c
+    // d - e - f
+    // g - h - i
+    // === ('e' is the current texel) ===
+    vec3 a = texture(srcTexture, vec2(texCoord.x - x, texCoord.y + y)).rgb;
+    vec3 b = texture(srcTexture, vec2(texCoord.x,     texCoord.y + y)).rgb;
+    vec3 c = texture(srcTexture, vec2(texCoord.x + x, texCoord.y + y)).rgb;
+
+    vec3 d = texture(srcTexture, vec2(texCoord.x - x, texCoord.y)).rgb;
+    vec3 e = texture(srcTexture, vec2(texCoord.x,     texCoord.y)).rgb;
+    vec3 f = texture(srcTexture, vec2(texCoord.x + x, texCoord.y)).rgb;
+
+    vec3 g = texture(srcTexture, vec2(texCoord.x - x, texCoord.y - y)).rgb;
+    vec3 h = texture(srcTexture, vec2(texCoord.x,     texCoord.y - y)).rgb;
+    vec3 i = texture(srcTexture, vec2(texCoord.x + x, texCoord.y - y)).rgb;
+
+    // Apply weighted distribution, by using a 3x3 tent filter:
+    //  1   | 1 2 1 |
+    // -- * | 2 4 2 |
+    // 16   | 1 2 1 |
+    upsample = e*4.0;
+    upsample += (b+d+f+h)*2.0;
+    upsample += (a+c+g+i);
+    upsample *= 1.0 / 16.0;
+}
